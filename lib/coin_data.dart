19a8c7efc24c030fa1f0bd0dacc8a9678d89c47b
Ghulam Hazrat <hazrat.sultani2022@gmail.com>
2023-06-15 22:33:12 -0700

    in this step i created a class the name coinData and used an asynch method and using for and if conditions i calculated the logic of work

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -30,3 +30,21 @@ const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
 const apiKey = 'YOUR-API-KEY-HERE';
 
 
+class CoinData {
+  Future getCoinData(String selectedCurrency) async {
+    Map<String, String> cryptoPrices = {};
+    for (String crypto in cryptoList) {
+      String requestURL =
+          '$coinAPIURL/$crypto/$selectedCurrency?apikey=$apiKey';
+      http.Response response = await http.get(requestURL);
+      if (response.statusCode == 200) {
+        var decodedData = jsonDecode(response.body);
+        double price = decodedData['rate'];
+        cryptoPrices[crypto] = price.toStringAsFixed(0);
+      } else {
+        print(response.statusCode);
+        throw 'Problem with the get request';
+      }  }
+    return cryptoPrices;
+  }}
+
